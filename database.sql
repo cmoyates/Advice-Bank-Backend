85e941b83decf4156b84b229bfd9b1aed2b2a2f4
Cristopher Yates <cmoyates@gmail.com>
2021-09-24 21:17:22 -0230

    Set up database and organized a bit

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE advicebank;
+
+CREATE TABLE posts(
+    post_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    user_name TEXT NOT NULL,
+    user_img TEXT NOT NULL,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    tags TEXT [],
+    ts TIMESTAMP NOT NULL
+);
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    user_email TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    user_img TEXT NOT NULL,
+    is_admin BOOLEAN NOT NULL,
+    saved INT []
+);
